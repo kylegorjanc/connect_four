52951b22e8a4b073b4fcda80ccac6726dc60f4bb
Kyle Gorjanc <kylegorjanc@gmail.com>
2015-10-19 05:06:40 -0400

    made it through game beginnings. Still need to work out actual play and checking for winner.

--- script/app.js
+++ script/app.js
@@ -4,68 +4,224 @@
 *	Date: 10/15/2015
 *****************/
 $(function(){
-console.log('Lock \'n load, everypony!')
+console.log('Lock \'n load, everypony!');
+//at the start, there is a start screen that covers the game board. 
+//This screen will have "1 player start" and "2 player start" buttons.
+//We'll call this Screen 1.
+// **********************
+var startScreen = function() {
+	var $overlay = $('<div>')
+	$overlay.addClass('overlay').attr('id', 'start');
+	var $onePbutton = $('<button>');
+	$onePbutton.addClass('button').attr('id', 'onePB').html('1 PLAYER')
+	var $twoPbutton = $('<button>');
+	$twoPbutton.addClass('button').attr('id', 'twoPB').html('2 PLAYER')
+	$overlay.prepend($onePbutton);
+	$overlay.prepend($twoPbutton);
+	$('.container').prepend($overlay);
+	// $onePbutton.click(function(){
+	// 	$('#start').remove();
+	// 	player1Choose();
+	// 	computerChoose();
+	// });
+	$twoPbutton.click(function(){
+		$('#start').remove();
+		// player1Choose();
+		// player2Choose();
+	});
+};
+startScreen();
 
+// var ponies = [
+// 'FlutterShy',
+// 'Pinkie Pie',
+// 'Rainbow Dash', 
+// 'Twilight Sparkle'
+// ]
+
+// var player1Choose = function(){
+// 	var $overlay = $('<div>')
+// 	$overlay.addClass('overlay').attr('id', 'oneP');
+// 	$('.container').prepend($overlay);
+// 	 var ponyList = $('<ul>').attr('id', 'ponyList');
+// 	 	ponies.forEach(function(value){
+// 	 		var li = $('<li>');
+// 	 		li.text(value);
+// 	 		ponyList.append(li);
+// 	 	});
+// 	 $('#oneP').append(ponyList);
+
+
+// 	var $fs = $('<img id = "fs" src ="images/fs_big.png">');
+// 		$fs.click(function(){
+// 			alert("You chose FlutterShy!");
+// 			Player1.name = 'FlutterShy';
+// 			Player1.chip = 'images/flutterchip.png';
+// 		});
+// 	var $pp = $('<img id = "pp" src = "images/pp_big.png">');
+// 		$pp.click(function(){
+// 			alert("You chose Pinkie Pie!");
+// 		});
+// 	var $rd = $('<img id = "rd" src = "images/rd_big.png">');
+// 		$rd.click(function(){
+// 			alert("You chose Rainbow Dash!");
+// 			Player1.name = 'Rainbow Dash';
+// 			Player1.chip = 'images/rainbowchip.png';
+// 		});
+// 	var $ts = $('<img id = "ts" src = "images/ts_big.png">');
+// 		$ts.click(function(){
+// 			alert("You chose Twilight Sparkle!");
+// 			Player1.name = 'Twilight Sparkle';
+// 			Player1.chip = 'images/twilightchip.png';
+// 		});
+// 	$('#oneP').append($fs, $pp, $rd, $ts);
+// 	$('#oneP').html('<ul><li>FlutterShy</li><li>Pinkie Pie</li><li>Rainbow Dash</li><li></ul>')
+// };
+
+
+// *******
 
 // To play the game you need two players, a game board, and some chips. Let's make those.
 //if 1 player mode, Player 2 === computer.
-Player = function(name, pony){
+// Player = function(name, chip, getPony){
+// 	this.name = name;
+// 	this.chip = "";
+// };
+Player = function(name, chip, num) {
 	this.name = name;
-	this.pony = pony;
-}
+	this.chip = chip;
+	this.num = num;
+};
 
-Player1 = new Player(Player1, getPony());
-Player2 = new Player(Player2, getPony());
+Player1 = new Player('Pinkie Pie', 'images/pinkiechip.png', 1);
+Player2 = new Player('Rainbow Dash', 'images/rainbowchip.png', 2);
+currentPlayer = Player1;
+// {
+// 	this.name = 'Pinkie Pie',
+// 	this.chipImg = '../images/pinkiechip.png'
+	
+// };
 
-//A cell needs to have a value
-// accept a new value when played
-// not accept new values when not played
-Cell = function() {
+// Player2 = function(name, chip) {
+// 	return{
+// 	name: 'Rainbow Dash',
+// 	chip: ''
+// 	chipImg: '../images/rainbowchip.png'
+// 	};
+// };
+/*******************LOAD GAME************/
+Game();
+
+
+});
+
+Cell = function(value) {
 	this.value = 0;
-	this.getValue = function() {
-	}
 	this.canPlay = function() {
+		if (this.value === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+};
 
-	}
-}
-
-//A column needs to have 6 cells.
-//if all 6 are full, it must tell you "no more!"
-//else if !full...
-//A column will only have one cellInPlay: the bottom-most cell.
-
-Column = function() {
-	this.canPlay = true;
+Column = function(cells, makeRows) {
 	this.cells = [];
-	this.cellInPlay = cells[0];
-}
+	this.makeRows = function() {
+		for (var i = 0; i < 6; i++){
+			(this.cells).push(new Cell());
+			this.cells[i].id = i.toString();
+			this.cells[i].className = "jCell";
+			// console.log(this.cells);			
+		}
+	};
+	this.makeRows();
+};
 
-//A board must have 7 Columns.
-//must determine if 1 player mode or 2 player mode
-//must start with Player 1 as the current player.
-//must place a click event on a Column.
-//The click will put the current player's chip on that Column's cell in play.
-//The click will remove the cell in Play from the Column.cells array.
-//The board will check for a Winner
-	//if winner is found, show the Winner Board
-//The current player will switch to the other player.
+//make the Columns out of cells.
 
-Board = function() {
+
+Board = function(columns) {
 	this.columns = [];
-	this.cells = [];
-	this.playerMode = playerMode;
-	this.currentPlayer = "";
-	this.winner = "";
-}
+	this.currentPlayer = Player1;
+};
+Board.prototype.dropChip = function() {
+		$('.column').each(function(){
+		  	$(this).click(function(){
+		  		var $last = $(this).children().last();
+		  		// if ($last.children().length > 0)
+				$last.append('<img id = ' + currentPlayer + ' src = '+ currentPlayer.chip + '>');
+				console.log($last);
+
+				// var x = $last.index;
+				// console.log(x);
+				// var y = $(this).index;
+				// console.log(y);
+				// newGame.columns[x][y].value = currentPlayer.num;
+				// console.log('click-ed.');
+				// console.log(currentPlayer.chip);
+				if (currentPlayer === Player1) {
+					currentPlayer = Player2;
+				} else if (currentPlayer === Player2) {
+					currentPlayer = Player1;
+				}
+				// console.log(currentPlayer);	
+				event.stopPropagation();	
+			})
+			// console.log($(this));
+		});
+// 		var col = document.getElementsbyClassName('column');
+// 		col.addEventListener('click', function(){
+// 			var cellImg = document.col.getElementsbyTagName('img'); 
+// 			cellImg.src = currentPlayer.chip;
+// 			console.log('I am clicked.')
+// 		});
+};
 
 Board.prototype.makeBoard = function(){
-	var $colDivs = $('.column');
-	if (this.columns = []) {
-		$cellDivs.each(value){
-			this.cells.push(new Column);
-		};
-	} 
+	for (var i=0; i < 7; i++) {
+		this.columns.push(new Column());
+		iString = i.toString();
+		this.columns[i].id = iString;
+		this.columns[i].className = "jColumn";
+		// var $colDiv = $('div');
+		// $colDiv.addClass('column').attr('id', iString);
+		
+	}			
+};
+
+Board.prototype.checkWinner = function() {
+	console.log('I need to finish this.')
 }
+
+Game = function(){
+	this.newGame = new Board();
+	this.newGame.makeBoard();
+	this.newGame.dropChip();
+	var gameArray = $.makeArray(this.newGame);
+	$.map(gameArray, this.newGame.checkWinner);
+};
+
+
+
+// Board.prototype.getMode = function(){
+
+// };
+
+
+// Board.prototype.getWinner = function(){
+    
+// };
+
+/* Okay.  The game itself is a function of the board and a player's chips. */
+// Game = function(board, chips) {
+// 	this.board = Board;
+// 	this.chips = Board.currentPlayer.chips
+
+// }
+
+
 
 
 
@@ -74,14 +230,29 @@ Board.prototype.makeBoard = function(){
 //at the start, there is a start screen that covers the game board. 
 //This screen will have "1 player start" and "2 player start" buttons.
 //We'll call this Screen 1.
-var startScreen = function(){
-	var field = $(<div>).overlay.attr('id', 'start');
-	var onePlayerButton = $(<button>).button.attr('id', 'onePlayerButton');
-	var twoPlayerButton = $(<button>).button.attr('id', 'twoPlayerButton');
-	field.append(onePlayerButton);
-	field.append(twoPlayerButton);
-}
-// Screen 2  //Player1Choose
+/***********************
+var startScreen = function() {
+	var $overlay = $('<div>').overlay.attr('id', 'start');
+	var $onePlayerButton = $('<button>').button.attr('id', 'onePlayerButton');
+	var $twoPlayerButton = $('<button>').button.attr('id', 'twoPlayerButton');
+	$image
+	$overlay.append($onePlayerButton);
+	$overlay.append($twoPlayerButton);
+	$('.board').prepend($overlay);
+};
+/***********************/
+// Screen 2  //player1Choose
+/***********************
+var player1Choose = function() {
+	var $overlay = $('<div>').overlay.attr('id', 'start');
+	var $onePlayerButton = $('<button>').button.attr('id', 'onePlayerButton');
+	var $twoPlayerButton = $('<button>').button.attr('id', 'twoPlayerButton');
+	$image
+	$overlay.append($onePlayerButton);
+	$overlay.append($twoPlayerButton);
+	$('.board').prepend($overlay);
+};
+/***********************/
 	//Player 1 chooses a pony.
 	//getPony = function() {
 		// all four ponies are shown. 
@@ -94,9 +265,3 @@ var startScreen = function(){
 // Screen 4 - game play.
 // Screen 5 WinnerScreen- announce the winner, ask 'do you want to play again?'
 //			If yes - back to screen 2.  Else, load the MLP homepage.
-
-
-
-
-
-})
